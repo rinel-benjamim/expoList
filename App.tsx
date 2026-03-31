7345fe0bfbfa7196979f8ef563383bb4de00249b
Rinel Benjamim <benjamimrinel192@gmail.com>
2026-03-31 21:26:57 +0100

    Add Login component and update app icon

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './src/pages/login';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello, World!</Text>
       <StatusBar style="auto" />
+      <Login />
     </View>
   );
 }
