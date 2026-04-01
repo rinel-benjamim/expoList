0a530be98562f7279fd064a53a63eefe82df0ed3
Rinel Benjamim <benjamimrinel192@gmail.com>
2026-04-01 10:32:53 +0100

    Refactor App component layout and add Login page with styles

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,10 +1,33 @@
 import React from "react";
 import {
-    Text
+  Text,
+  View,
+  Image,
+  TextInput
 } from 'react-native';
+
+import Logo from "../../assets/logo.png";
+
+import { style } from "./styles";
 
 export default function Login() {
   return (
-    <Text>Olá mundo 2!</Text>
+    <View style={style.container}>
+      <View style={style.boxTop}>
+        <Image
+          source={Logo}
+        />
+        <Text>Bem vindo de volta!</Text>
+      </View>
+      <View style={style.boxMiddle}>
+        <Text>Endereço de e-mail</Text>
+        <TextInput />
+        <Text>Palavra-passe</Text>
+        <TextInput />
+      </View>
+      <View style={style.boxBottom}>
+        <Text>Baixo</Text>
+      </View>
+    </View >
   );
 }
---
+++ src/pages/login/styles.ts
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+export const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    boxTop: {
+        height: 200,
+        backgroundColor: 'red',
+        width: '100%',
+    },
+
+    boxMiddle: {
+        height: 200,
+        backgroundColor: 'blue',
+        width: '100%',
+    },
+
+    boxBottom: {
+        height: 200,
+        backgroundColor: 'green',
+        width: '100%',
+    }
+})
